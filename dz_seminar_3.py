cd532741bd0bcd5419848160d5dcc5b31e6484aa
justicha <haquangminh@mail.ru>
2026-03-04 03:17:50 +0300

    Refactor interval data and enhance function logic
    Updated intervals and modified function to track start times more effectively.

--- dz_seminar_3.py
+++ dz_seminar_3.py
@@ -1,20 +1,23 @@
 #Взвешенное интервальное планирование
-intervals = [
-    (0, 3, 3), 
-    (1, 4, 2), 
-    (3, 5, 4),  
-    (4, 7, 2), 
-    (5, 8, 3)    
-]
+intervals = [(0, 3, 3), (0, 2, 2), (3, 5, 4), (4, 7, 2), (0, 1, 3)]
 
 def f(n):
+    start_number = {}
     c = 0
-    d = n[0][0] - 1
+    
     for i in n:
-        b = []
-        b.append(i[0]+i[1])
-        if d <= i[0]:
-            c += i[2]
-            d = i[1]
+        start = i[0]
+        dlina = i[1] - i[0]
+        
+        if start not in start_number or dlina < start_number[start][1] - start_number[start][0]:
+            start_number[start] = i
+    
+    info = list(start_number.values())
+    print(info)
+
+    for i in info:
+        c += i[2]
+    
     return f"максимальный вес = {c}"
+
 f(intervals)
